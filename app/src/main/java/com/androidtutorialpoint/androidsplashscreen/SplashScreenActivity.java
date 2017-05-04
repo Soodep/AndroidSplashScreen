3a35ef5b3f64ee889030f0f048486d9a8c76171d
Soodep <soodepdhakal@gmail.com>
2017-05-04 14:32:47 +0545

    Initial commit

---
+++ app/src/main/java/com/androidtutorialpoint/androidsplashscreen/SplashScreenActivity.java
@@ -0,0 +1,105 @@
+package com.androidtutorialpoint.androidsplashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by navneet on 12/11/16.
+ */
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreenmaker);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        final TextView txt = (TextView) findViewById(R.id.txt);
+        final TextView txt1 = (TextView) findViewById(R.id.txt1);
+        final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        final Animation animation_2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.antirotate);
+        final Animation animation_3 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+        final Animation animation_4 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.alpha);
+        final Animation animation_5 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.bottom_up);
+
+        txt1.setAnimation(animation_5);
+        animation_5.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        txt.startAnimation(animation_4);
+        animation_4.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        imageView.startAnimation(animation_2);
+        animation_2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(animation_1);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        animation_1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(animation_3);
+                finish();
+                Intent i = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
